7559af518ce067809e9e736b300cb117d3f3d34e
Vadim Zubovsky <vadimzubovski2525@gmail.com>
2022-10-11 13:35:21 +0300

    Create helpers.ts with two reduce function

---
+++ app/utils/helpers/helpers.ts
@@ -0,0 +1,11 @@
+export const reduceNumber = (num: number) => Math.round(num * 100) / 100;
+
+export const reduceMoney = (num: number) => {
+  if (num >= 1000000000) {
+    return Math.round(num / 10000000) / 100 + "b";
+  } else if (num >= 1000000) {
+    return Math.round(num / 10000) / 100 + "m";
+  } else {
+    return reduceNumber(num);
+  }
+};
