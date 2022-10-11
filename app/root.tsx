dbe14c3843bffb7ad5118ea7d6278c0975b66be6
Vadim Zubovsky <vadimzubovski2525@gmail.com>
2022-10-11 10:20:10 +0300

    Add global styles

--- app/root.tsx
+++ app/root.tsx
@@ -1,4 +1,4 @@
-import type { MetaFunction } from "@remix-run/node";
+import type { LinksFunction, MetaFunction } from "@remix-run/node";
 import {
   Links,
   LiveReload,
@@ -7,12 +7,37 @@ import {
   Scripts,
   ScrollRestoration,
 } from "@remix-run/react";
+import { createGlobalStyle } from "styled-components";
+
+export const links: LinksFunction = () => {
+  return [
+    { rel: "preconnect", href: "https://fonts.googleapis.com" },
+    {
+      rel: "preconnect",
+      href: "https://fonts.gstatic.com",
+      crossorigin: "anonymous",
+    },
+    {
+      href: "https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap",
+      rel: "stylesheet",
+    },
+  ];
+};
 
 export const meta: MetaFunction = () => ({
   charset: "utf-8",
   title: "Cryptocurrency statistics",
   viewport: "width=device-width,initial-scale=1",
 });
+
+const Global = createGlobalStyle`
+  *{
+    margin :0;
+    padding: 0;
+    box-sizing: border-box;
+    font-family: 'Roboto', sans-serif;
+  }
+`;
 
 export default function App() {
   return (
@@ -22,6 +47,7 @@ export default function App() {
         <Links />
       </head>
       <body>
+        <Global />
         <Outlet />
         <ScrollRestoration />
         <Scripts />
