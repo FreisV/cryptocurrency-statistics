22f956049ae41d924e0c2ea2db74e9b1b4e44006
Vadim Zubovsky <vadimzubovski2525@gmail.com>
2022-10-12 12:39:51 +0300

    Add row and col styled components

--- app/components/styles.ts
+++ app/components/styles.ts
@@ -21,3 +21,29 @@ export const RedSpan = styled.span`
 export const GreenSpan = styled.span`
   color: #18c683;
 `;
+
+type FlexProps = {
+  width?: string;
+  height?: string;
+  align?: string;
+  justify?: string;
+}
+
+export const Row = styled.div<FlexProps>`
+  display: flex;
+  flex-direction: row;
+  
+  width: ${props => props.width ? props.width : 'auto'};
+  height: ${props => props.height ? props.height : 'auto'};;
+  align-items: ${props => props.align ? props.align : 'initial'};
+  justify-content: ${props => props.justify ? props.justify : 'space-between'};
+`
+export const Col = styled.div<FlexProps>`
+  display: flex;
+  flex-direction: column;
+
+  width: ${props => props.width ? props.width : 'auto'};
+  height: ${props => props.height ? props.height : 'auto'};;
+  align-items: ${props => props.align ? props.align : 'initial'};
+  justify-content: ${props => props.justify ? props.justify : 'space-between'};
+`
