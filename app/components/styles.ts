d89017ded2111a79527a7d1faf351ed087ba041a
Vadim Zubovsky <vadimzubovski2525@gmail.com>
2022-10-12 09:28:32 +0300

    Add common styled elements

---
+++ app/components/styles.ts
@@ -0,0 +1,23 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const Main = styled.main`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  width: 1440px;
+`
+
+export const RedSpan = styled.span`
+  color: #f44336;
+`;
+
+export const GreenSpan = styled.span`
+  color: #18c683;
+`;
