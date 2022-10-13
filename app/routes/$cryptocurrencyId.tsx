80976940156bfbe5ac316faca4904f4ba861714d
Vadim Zubovsky <vadimzubovski2525@gmail.com>
2022-10-13 21:33:32 +0300

    Create Parent page for CryptocurrencyInfo

---
+++ app/routes/$cryptocurrencyId.tsx
@@ -0,0 +1,22 @@
+import type { LoaderFunction } from "@remix-run/node";
+import { Outlet, useLoaderData } from "@remix-run/react";
+import type { CryptocurrencyType } from "~/api/cryptocurrencies";
+import { getTopThreeCryptocurrencies } from "~/api/cryptocurrencies";
+import Header from "~/components/Header";
+import { Wrapper } from "~/components/styles";
+
+export const loader: LoaderFunction = () => {
+  return getTopThreeCryptocurrencies();
+};
+
+const Cryptocurrency = () => {
+  const topThree = useLoaderData<CryptocurrencyType[]>();
+  return (
+    <Wrapper>
+      <Header topThree={topThree} />
+      <Outlet />
+    </Wrapper>
+  );
+};
+
+export default Cryptocurrency;
