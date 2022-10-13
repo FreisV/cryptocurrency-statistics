2309975bb4591af2822dff789e3bc3b87f7f60ff
Vadim Zubovsky <vadimzubovski2525@gmail.com>
2022-10-13 21:31:50 +0300

    Refactor code to send props into Header

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,21 +1,30 @@
 import CryptocurrencyTable from "~/components/CryptocurrencyTable";
-import type { CryptocurrencyType } from "~/api/cryptocurrencies";
+import type { CryptocurrencyType} from "~/api/cryptocurrencies";
+import { getTopThreeCryptocurrencies } from "~/api/cryptocurrencies";
 import { getCryptocurrencies } from "~/api/cryptocurrencies";
 import type { LoaderFunction } from "@remix-run/node";
 import { useLoaderData } from "@remix-run/react";
 import { Main, Wrapper } from "~/components/styles";
 import Header from "~/components/Header";
 
+type LoaderType = {
+  cryptocurrencies: CryptocurrencyType[];
+  topThree: CryptocurrencyType[];
+} 
+
 export const loader: LoaderFunction = async () => {
-  return getCryptocurrencies();
+  const cryptocurrencies = getCryptocurrencies();
+  const topThree = getTopThreeCryptocurrencies();
+
+  return {cryptocurrencies, topThree};
 };
 
 const Index = () => {
-  const cryptocurrencies = useLoaderData<Array<CryptocurrencyType>>();
+  const {cryptocurrencies, topThree} = useLoaderData<LoaderType>();
 
   return (
     <Wrapper>
-      <Header/>
+      <Header topThree={topThree}/>
       <Main>
         <CryptocurrencyTable cryptocurrencies={cryptocurrencies} />
       </Main>
