542a1aa111a2955cd79898b9751f78b73594748e
Vadim Zubovsky <vadimzubovski2525@gmail.com>
2022-10-14 10:39:20 +0300

    Add Links and adaptive

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -2,41 +2,69 @@ import styled from "styled-components";
 import type { CryptocurrencyType } from "~/api/cryptocurrencies";
 import { reduceMoney } from "~/utils/helpers/helpers";
 import { Row, StyledHeader } from "./styles";
+import { Link } from "@remix-run/react";
+
 
 type HeaderProps = {
   topThree: CryptocurrencyType[];
 }
 
 const Logo = styled.h1`
-  padding: 10px;
+  padding: 10px 30px;
+  text-decoration: none;
 `;
 
+const StyledLink = styled(Link)`
+  color: #3b3b3b;
+  text-decoration: none;
+`;
+
+const Nav = styled.nav`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+`
+
 const Profile = styled.span`
-  padding: 10px;
+  padding: 10px 30px;
   font-size: 1.2em;
   font-weight: 500;
+  align-self: right;
 `;
+
+const Cryptocurrencies = styled(Row)`
+
+  @media (max-width: 600px) {
+    display: none;
+  }
+`
 
 const Cryptocurrency = styled.span`
   padding: 15px;
   font-size: 0.95em;
 `;
 
+
+
+
 const Header = ({topThree}: HeaderProps) => {
   return (
     <StyledHeader>
       <Row justify="space-between" align="center" maxWidth="1440px" width="100%">
-        <Logo>CoinCap</Logo>
-        <Row justify="space-between" align="center" width="60%">
-          <Row>
+        <Logo><StyledLink to="/" prefetch="render">CoinCap</StyledLink></Logo>
+        <Nav>
+          <Cryptocurrencies>
             {topThree && topThree.map((cryptocurrency) => (
               <Cryptocurrency key={cryptocurrency.id}>
-                {cryptocurrency.symbol}: ${reduceMoney(parseFloat(cryptocurrency.priceUsd))}
+                <StyledLink to={'/'+cryptocurrency.id} prefetch="intent">
+                  {cryptocurrency.symbol}: ${reduceMoney(parseFloat(cryptocurrency.priceUsd))}
+                </StyledLink>
               </Cryptocurrency>
             ))}
-          </Row>
+          </Cryptocurrencies>
           <Profile>Портфель</Profile>
-        </Row>
+        </Nav>
       </Row>
     </StyledHeader>
   );
