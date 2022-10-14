4c44d12979a412c6c206ea80088588ba4f260761
Vadim Zubovsky <vadimzubovski2525@gmail.com>
2022-10-14 14:55:47 +0300

    Add styles to paginator

--- app/components/Pagination.tsx
+++ app/components/Pagination.tsx
@@ -1,13 +1,34 @@
 import { Link } from "@remix-run/react";
+import styled from "styled-components";
 import { Row } from "./styles";
 
 type PaginationProps = {
   currentPage: number;
 };
 
-const Pagination = ({ currentPage }: PaginationProps) => {
+const PaginationRow = styled(Row)`
+  margin: 20px;
+`
 
-  
+const PageBtn = styled.div`
+  width: 29px;
+  height: 29px;
+  padding: 5px;
+  border-radius: 50%;
+  text-align: center;
+
+  &:hover {
+    cursor: pointer;
+    text-decoration: underline;
+  }
+`
+
+const CurrentPageBtn = styled(PageBtn)`
+  background-color: #bebebe;
+  font-weight: 500;
+`
+
+const Pagination = ({ currentPage }: PaginationProps) => {
   const range = {
     from: 1,
     to: 45,
@@ -22,22 +43,19 @@ const Pagination = ({ currentPage }: PaginationProps) => {
   if(currentPage < range.from || currentPage > range.to) {
     throw new Error('Incorrect current page');
   }
-  const pages = [...range];
 
   return (
-    <Row>
-      <Row>
-          <div>{range.from}</div>
-          {currentPage - 3 > range.from ? <div>...</div> : <></> }
-          {currentPage - 2 > range.from ? <div>{currentPage - 2}</div> : <></>}
-          {currentPage - 1 > range.from ? <div>{currentPage - 1}</div> : <></>}
-          {currentPage > range.from && currentPage < range.to ? <div>{currentPage}</div> : <></>}
-          {currentPage + 1 < range.to ? <div>{currentPage + 1}</div> : <></>}
-          {currentPage + 2 < range.to ? <div>{currentPage + 2}</div> : <></>}
-          {currentPage + 3 < range.to ? <div>...</div> : <></> }
-          <div>{range.to}</div>
-        </Row>
-    </Row>
+    <PaginationRow>
+          {currentPage === range.from ? <CurrentPageBtn>{range.from}</CurrentPageBtn> : <PageBtn>{range.from}</PageBtn>}
+          {currentPage - 3 > range.from ? <PageBtn>...</PageBtn> : <></> }
+          {currentPage - 2 > range.from ? <PageBtn>{currentPage - 2}</PageBtn> : <></>}
+          {currentPage - 1 > range.from ? <PageBtn>{currentPage - 1}</PageBtn> : <></>}
+          {currentPage > range.from && currentPage < range.to ? <CurrentPageBtn>{currentPage}</CurrentPageBtn> : <></>}
+          {currentPage + 1 < range.to ? <PageBtn>{currentPage + 1}</PageBtn> : <></>}
+          {currentPage + 2 < range.to ? <PageBtn>{currentPage + 2}</PageBtn> : <></>}
+          {currentPage + 3 < range.to ? <PageBtn>...</PageBtn> : <></> }
+          {currentPage === range.to ? <CurrentPageBtn>{range.to}</CurrentPageBtn> : <PageBtn>{range.to}</PageBtn>}
+    </PaginationRow>
   );
 };
 
