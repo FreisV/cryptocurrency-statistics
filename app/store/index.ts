28d02005b1c821731f22e893214438d0fb4a6363
Vadim Zubovsky <vadimzubovski2525@gmail.com>
2022-10-18 01:46:46 +0300

    Add save in local storage

--- app/store/index.ts
+++ app/store/index.ts
@@ -1,6 +1,34 @@
-import { configureStore } from '@reduxjs/toolkit'
-import { rootReducer } from './reducers'
+import { configureStore } from "@reduxjs/toolkit";
+import type { RootState } from "./reducers";
+import { rootReducer } from "./reducers";
+
+const saveToLocalStorage = (state: RootState) => {
+  try {
+    localStorage.setItem("coincapApp", JSON.stringify(state));
+  } catch (e) {
+    console.error(e);
+  }
+};
+
+const loadFromLocalStorage = () => {
+  if (typeof window !== "undefined") {
+    try {
+      const stateStr = localStorage.getItem("coincapApp");
+      return stateStr ? JSON.parse(stateStr) : undefined;
+    } catch (e) {
+      return undefined;
+    }
+  }
+};
+
+const preloadedState = loadFromLocalStorage();
 
 export const store = configureStore({
   reducer: rootReducer,
-})
+  preloadedState: preloadedState,
+});
+
+store.subscribe(() => {
+  console.log("Test");
+  saveToLocalStorage(store.getState());
+});
