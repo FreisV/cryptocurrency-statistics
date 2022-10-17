6eb557abb12356a4ab7100acde36b87863cf8b53
Vadim Zubovsky <vadimzubovski2525@gmail.com>
2022-10-17 21:29:58 +0300

    Add adding cryptocurrency into briefcase

--- app/components/AddCryptocurrencyModal.tsx
+++ app/components/AddCryptocurrencyModal.tsx
@@ -1,6 +1,8 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { useDispatch } from "react-redux";
 import styled from "styled-components";
 import type { CryptocurrencyType } from "~/api/cryptocurrencies";
+import { addCryptocurrency } from "~/store/reducers/briefcaseReducer";
 import { reduceMoney } from "~/utils/helpers/helpers";
 import {
   B,
@@ -34,10 +36,20 @@ const StyledCol = styled(Col)`
 `
 
 const AddCryptocurrencyModal = ({cryptocurrency, isHide, setIsHide}: AddCryptocurrencyModalProps) => {
+  const [quantity, setQuantity] = useState(0);
   const [sum, setSum] = useState(0);
   const name = cryptocurrency.name;
   const price = cryptocurrency.priceUsd === null ? 0 : reduceMoney(parseFloat(cryptocurrency.priceUsd || '0'))
+  const dispatch = useDispatch();
   
+  useEffect(() => {
+    setSum(quantity *  parseFloat(cryptocurrency.priceUsd || '0'));
+  }, [cryptocurrency.priceUsd, quantity])
+
+  const addInBriefcase = (cryptocurrency:CryptocurrencyType, quantity:number) => {
+    const purchasePrice = quantity * parseFloat(cryptocurrency.priceUsd || '0');
+    dispatch(addCryptocurrency({cryptocurrency, quantity, purchasePrice}));
+  }
 
   if(isHide){
     return null;
@@ -52,13 +64,13 @@ const AddCryptocurrencyModal = ({cryptocurrency, isHide, setIsHide}: AddCryptocu
           <Col>
             <InfoSpan><B>Cryptocurrency: </B> {name}</InfoSpan>
             <InfoSpan><B>Price: </B>$ {price}</InfoSpan>
-            <ModalForm>
+            <ModalForm onSubmit={() => addInBriefcase(cryptocurrency, quantity)}>
               <StyledCol width="100%">
                 <NumberInput
                   type="number"
                   step="any"
                   placeholder="Amount of cryptocurrency"
-                  onChange={e => setSum(Number(e.target.value) *  parseFloat(cryptocurrency.priceUsd || '0'))}
+                  onChange={e => setQuantity(Number(e.target.value))}
                 />
                 <InfoSpan><B>Sum: </B>$ {sum === 0 ? 0 : reduceMoney(sum)}</InfoSpan>
                 <Button>Add</Button>
