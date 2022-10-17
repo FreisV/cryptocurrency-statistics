6d580360b19e660112233d44cfb353519e816e4f
Vadim Zubovsky <vadimzubovski2525@gmail.com>
2022-10-17 09:25:29 +0300

    Add subtract function and add checking for negative values

--- app/store/reducers/briefcaseReducer.ts
+++ app/store/reducers/briefcaseReducer.ts
@@ -1,5 +1,6 @@
 import type { BriefcaseAction, BriefcaseState, CryptocurrencyInBriefcaseType } from "~/types/briefcase";
 import { BriefcaseActionTypes } from "~/types/briefcase";
+import { reduceNumber } from "~/utils/helpers/helpers";
 
 
 const initialState: BriefcaseState = {
@@ -7,6 +8,10 @@ const initialState: BriefcaseState = {
 };
 
 const correctAddCryptocurrency = (state: BriefcaseState, action: BriefcaseAction) :CryptocurrencyInBriefcaseType[] => {
+  if (action.payload.quantity <= 0) {
+    return state.briefcase.map(el => structuredClone(el));
+  }
+
   const cryptocurrencies =  state.briefcase.map(el => structuredClone(el));
 
   const indexCryptocurrency = cryptocurrencies.findIndex(el => el.cryptocurrency.id === action.payload.cryptocurrency.id)
@@ -21,8 +26,37 @@ const correctAddCryptocurrency = (state: BriefcaseState, action: BriefcaseAction
 
   cryptocurrencies[indexCryptocurrency] = cryptocurrency;
 
-  console.log(cryptocurrencies);
-  
+  return cryptocurrencies
+}
+
+const correctSubtractCryptocurrency = (state: BriefcaseState, action: BriefcaseAction) :CryptocurrencyInBriefcaseType[] => {
+  if (action.payload.quantity <= 0) {
+    return state.briefcase.map(el => structuredClone(el));
+  }
+
+
+  const cryptocurrencies =  state.briefcase.map(el => structuredClone(el));
+
+  const indexCryptocurrency = cryptocurrencies.findIndex(el => el.cryptocurrency.id === action.payload.cryptocurrency.id)
+
+  if (indexCryptocurrency === -1) {
+    return cryptocurrencies;
+  }
+
+  const cryptocurrency = cryptocurrencies[indexCryptocurrency]
+
+  if (cryptocurrency.quantity <=  action.payload.quantity) {
+    return cryptocurrencies.filter(el => el.cryptocurrency.id !== cryptocurrency.cryptocurrency.id)
+  }
+
+  //Shows how many percent of the cryptocurrency is left of the previous amount
+  const procent = 100 - (action.payload.quantity / (cryptocurrency.quantity / 100))
+
+  cryptocurrency.purchasePrice = cryptocurrency.purchasePrice / 100 * procent;
+  cryptocurrency.quantity = reduceNumber(cryptocurrency.quantity - action.payload.quantity);
+
+  cryptocurrencies[indexCryptocurrency] = cryptocurrency;
+
   return cryptocurrencies
 }
 
@@ -30,9 +64,12 @@ export const briefcaseReducer = (state = initialState, action: BriefcaseAction) 
   switch (action.type) {
     case BriefcaseActionTypes.ADD_CRYPTOCURRENCY: 
       return {...state, briefcase: correctAddCryptocurrency(state,action)}
+    case BriefcaseActionTypes.SUBTRACT_CRYPTOCURRENCY:
+      return {...state, briefcase: correctSubtractCryptocurrency(state,action)}
     default:
       return state;
   }
 };
 
 export const addCryptocurrency = (payload: CryptocurrencyInBriefcaseType) => ({type: BriefcaseActionTypes.ADD_CRYPTOCURRENCY, payload})
+export const subtractCryptocurrency = (payload: CryptocurrencyInBriefcaseType) => ({type: BriefcaseActionTypes.SUBTRACT_CRYPTOCURRENCY, payload})
