32c9b30e156c26da6bf6a1392ff84024d5500d6e
Vadim Zubovsky <vadimzubovski2525@gmail.com>
2022-10-18 00:37:10 +0300

    Add briefcase modal items

--- app/components/BriefcaseModal.tsx
+++ app/components/BriefcaseModal.tsx
@@ -1,18 +1,12 @@
 import styled from "styled-components";
 import { useTypedSelector } from "~/hooks/useTypedSelector";
-import { reduceMoney } from "~/utils/helpers/helpers";
+import BriefcaseModalItem from "./BriefcaseModalItem";
 import {
-  B,
-  Button,
   Col,
-  InfoSpan,
   Modal,
   ModalBody,
   ModalContent,
-  ModalForm,
   ModalHeader,
-  NumberInput,
-  StyledCol
 } from "./styles";
 
 type BriefcaseModalProps = {
@@ -24,15 +18,6 @@ const BriefcaseInfo = styled(Col)`
   padding: 5px 0 10px 0;
   align-items: center;
   justify-content: center;
-`;
-
-const BriefcaseItem = styled(Col)`
-  width: 100%;
-  padding: 10px 0;
-
-  &:not(:first-child) {
-    border-top: 1px solid #c4c4c4;
-  }
 `;
 
 const BriefcaseModal = ({ isHide, setIsHide }: BriefcaseModalProps) => {
@@ -49,34 +34,9 @@ const BriefcaseModal = ({ isHide, setIsHide }: BriefcaseModalProps) => {
         <ModalBody>
           <BriefcaseInfo>
             {briefcase.length === 0
-              ? "Портфель пуст"
-              : briefcase.map((el) => {
-                  const purchasePrice = reduceMoney(el.purchasePrice)
-
-                  return (
-                    <BriefcaseItem key={el.cryptocurrency.id}>
-                      <InfoSpan>
-                        <B>Cryptocurrency: </B> {el.cryptocurrency.name}
-                      </InfoSpan>
-                      <InfoSpan>
-                        <B>Quantity: </B> {el.quantity}
-                      </InfoSpan>
-                      <InfoSpan>
-                        <B>Purchase price: </B>$ {purchasePrice}
-                      </InfoSpan>
-                      <ModalForm>
-                        <StyledCol>
-                          <NumberInput
-                            type="number"
-                            step="any"
-                            placeholder="Amount of cryptocurrency"
-                          />
-                          <Button>Remove</Button>
-                        </StyledCol>
-                      </ModalForm>
-                    </BriefcaseItem>
-                  );
-                })}
+              ? "Briefcase empty"
+              : briefcase.map(el => <BriefcaseModalItem item={el} key={el.cryptocurrency.id}/>
+              )}
           </BriefcaseInfo>
         </ModalBody>
       </ModalContent>
