ad2ca1f5888c09b1b1c72f7a3dc865abdd51501c
Vadim Zubovsky <vadimzubovski2525@gmail.com>
2022-10-18 13:11:37 +0300

    Add more action types

--- app/types/briefcase.ts
+++ app/types/briefcase.ts
@@ -12,10 +12,23 @@ export type BriefcaseState = {
 
 export enum BriefcaseActionTypes {
   ADD_CRYPTOCURRENCY = "ADD_CRYPTOCURRENCY",
-  SUBTRACT_CRYPTOCURRENCY = "SUBTRACT_CRYPTOCURRENCY"
+  SUBTRACT_CRYPTOCURRENCY = "SUBTRACT_CRYPTOCURRENCY",
+  UPDATE_CRYPTOCURRENCIES = "UPDATE_CRYPTOCURRENCIES"
 }
 
-export type BriefcaseAction = {
-  type: string,
+export type AddCryptocurrencyAction = {
+  type: BriefcaseActionTypes.ADD_CRYPTOCURRENCY;
   payload: CryptocurrencyInBriefcaseType;
 }
+
+export type SubtractCryptocurrencyAction = {
+  type: BriefcaseActionTypes.SUBTRACT_CRYPTOCURRENCY;
+  payload: {cryptocurrency: CryptocurrencyType, quantity: number}
+}
+
+export type UpdateCryptocurrenciesAction = {
+  type: BriefcaseActionTypes.UPDATE_CRYPTOCURRENCIES;
+  payload: CryptocurrencyType[];
+}
+
+export type BriefcaseAction = AddCryptocurrencyAction | SubtractCryptocurrencyAction | UpdateCryptocurrenciesAction
