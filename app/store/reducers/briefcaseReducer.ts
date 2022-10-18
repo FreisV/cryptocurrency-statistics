aa6012a90aa8ff702c15167fb0c94bba53bf8bce
Vadim Zubovsky <vadimzubovski2525@gmail.com>
2022-10-18 19:30:01 +0300

    Fix import

--- app/store/reducers/briefcaseReducer.ts
+++ app/store/reducers/briefcaseReducer.ts
@@ -1,4 +1,4 @@
-import { CryptocurrencyType } from "~/api/cryptocurrencies";
+import type { CryptocurrencyType } from "~/api/cryptocurrencies";
 import type {
   AddCryptocurrencyAction,
   BriefcaseAction,
