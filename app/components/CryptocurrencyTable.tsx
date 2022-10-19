ca14d14c8568d1ec16a7e0d97764768375f2668a
Vadim Zubovsky <vadimzubovski2525@gmail.com>
2022-10-19 12:51:06 +0300

    Move cryptocurrency table styles to separate file

--- app/components/CryptocurrencyTable.tsx
+++ app/components/CryptocurrencyTable.tsx
@@ -1,70 +1,13 @@
-import { Link } from "@remix-run/react";
-import styled from "styled-components";
 import type { CryptocurrencyType } from "~/api/cryptocurrencies";
 import { reduceNumber, reduceMoney } from "../utils/helpers/helpers";
 import { GreenSpan, RedSpan } from "./styles";
 import { useState } from "react";
 import AddCryptocurrencyModal from "./AddCryptocurrencyModal";
+import { Add, StyledLink, Table, Td, Th, Tr } from "./cryptocurrencyTableStyles";
 
 type CryptocurrencyTableProps = {
   cryptocurrencies: CryptocurrencyType[];
 };
-
-const Table = styled.table`
-  width: 100%;
-  border-spacing: 0px;
-`;
-
-const Th = styled.th`
-  /*Adapt the text-align*/
-  text-align: ${(props) => (props.align ? props.align : "end")};
-
-  padding: 1em;
-  border-top: 1px solid #c4c4c4;
-  border-bottom: 1px solid #c4c4c4;
-  font-weight: 500;
-  font-size: 1em;
-  padding: 1em 1.5em;
-
-  &:nth-child(2) {
-    width: 20%;
-  }
-`;
-
-const Td = styled.td`
-  /*Adapt the text-align*/
-  text-align: ${(props) => (props.align ? props.align : "end")};
-
-  border-bottom: 1px solid #c4c4c4;
-  font-size: 0.9em;
-`;
-
-const Tr = styled.tr`
-  &:hover {
-    background-color: rgb(248, 250, 253);
-    cursor: pointer;
-  }
-`;
-
-const StyledLink = styled(Link)`
-  display: block;
-  padding: 1.5em;
-  color: black;
-  text-decoration: none;
-`;
-
-const Add = styled.button`
-  font-size: 1.5em;
-  font-weight: 300;
-  transition: font-size 100ms;
-  border: none;
-  background-color: rgba(255,255,255,0);
-
-  &:hover {
-    font-size: 2em;
-    cursor: pointer;
-  }
-`
 
 const CryptocurrencyTable = ({
   cryptocurrencies,
---
+++ app/components/cryptocurrencyTableStyles.ts
@@ -0,0 +1,58 @@
+import { Link } from "@remix-run/react";
+import styled from "styled-components";
+
+export const Table = styled.table`
+  width: 100%;
+  border-spacing: 0px;
+`;
+
+export const Th = styled.th`
+  /*Adapt the text-align*/
+  text-align: ${(props) => (props.align ? props.align : "end")};
+
+  padding: 1em;
+  border-top: 1px solid #c4c4c4;
+  border-bottom: 1px solid #c4c4c4;
+  font-weight: 500;
+  font-size: 1em;
+  padding: 1em 1.5em;
+
+  &:nth-child(2) {
+    width: 20%;
+  }
+`;
+
+export const Td = styled.td`
+  /*Adapt the text-align*/
+  text-align: ${(props) => (props.align ? props.align : "end")};
+
+  border-bottom: 1px solid #c4c4c4;
+  font-size: 0.9em;
+`;
+
+export const Tr = styled.tr`
+  &:hover {
+    background-color: rgb(248, 250, 253);
+    cursor: pointer;
+  }
+`;
+
+export const StyledLink = styled(Link)`
+  display: block;
+  padding: 1.5em;
+  color: black;
+  text-decoration: none;
+`;
+
+export const Add = styled.button`
+  font-size: 1.5em;
+  font-weight: 300;
+  transition: font-size 100ms;
+  border: none;
+  background-color: rgba(255,255,255,0);
+
+  &:hover {
+    font-size: 2em;
+    cursor: pointer;
+  }
+`
