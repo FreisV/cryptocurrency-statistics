aad99a53ca01872af5828be89db18a98e15393b0
Vadim Zubovsky <vadimzubovski2525@gmail.com>
2022-10-25 16:50:45 +0300

    Replace fetch with axios

--- app/api/cryptocurrencies.ts
+++ app/api/cryptocurrencies.ts
@@ -1,3 +1,5 @@
+import axios from "axios";
+
 export type CryptocurrencyType = {
   id: string;
   rank: string;
@@ -13,20 +15,20 @@ export type CryptocurrencyType = {
 };
 
 export const getCryptocurrencies = async (page: number) => {
-  const response = await fetch(`https://api.coincap.io/v2/assets?offset=${page * 50 - 50}&limit=50`);
-  const cryptocurrencies: CryptocurrencyType[] = (await response.json()).data;
+  const response = await axios.get(`https://api.coincap.io/v2/assets?offset=${page * 50 - 50}&limit=50`);
+  const cryptocurrencies: CryptocurrencyType[] = response.data.data;
   return cryptocurrencies;
 }
 
 export const getCryptocurrencyById = async (cryptocurrencyId: string) => {
-  const response = await fetch(`https://api.coincap.io/v2/assets/${cryptocurrencyId}`);
-  const cryptocurrencies: CryptocurrencyType = (await response.json()).data;
+  const response = await axios.get(`https://api.coincap.io/v2/assets/${cryptocurrencyId}`);
+  const cryptocurrencies: CryptocurrencyType = response.data.data;
   return cryptocurrencies;
 }
 
 export const getTopThreeCryptocurrencies = async () => {
-  const response = await fetch('https://api.coincap.io/v2/assets?limit=3');
-  const cryptocurrencies: CryptocurrencyType[] = (await response.json()).data;
+  const response = await axios.get('https://api.coincap.io/v2/assets?limit=3');
+  const cryptocurrencies: CryptocurrencyType[] = response.data.data;
   return cryptocurrencies;
 }
 
@@ -36,7 +38,7 @@ export type HistoryType = {
 }
 
 export const getHistoryById = async (cryptocurrencyId: string) => {
-  const response = await fetch(`https://api.coincap.io/v2/assets/${cryptocurrencyId}/history?interval=h1`)
-  const history: HistoryType[] = (await response.json()).data;
+  const response = await axios.get(`https://api.coincap.io/v2/assets/${cryptocurrencyId}/history?interval=h1`)
+  const history: HistoryType[] = response.data.data;
   return history;
 }
